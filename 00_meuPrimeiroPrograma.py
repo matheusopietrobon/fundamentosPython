1682b2e24faf054db97f882989ce3041f50ecb04
matheuspietrobon <mpietrobon@gmail.com>
2026-07-29 20:52:40 -0300

    Finalizando curso Pyhon

---
+++ 00_meuPrimeiroPrograma.py
@@ -0,0 +1,18 @@
+# Tipos de dados: 
+# int = Numeros inteiros, ex: 10; 5; 20; 33...
+# float = Numeros decimais, ex: 1,20; 3,33
+# string = Palavras e/ou caracteres, ex:"a", "oi", "Ola tudo bem?", "10"
+# booleano = Somente duas possibilidades: True ou False
+
+
+# nome = input("Ola, digite seu nome: ")
+# idade = input("Qual a sua idade? ")
+# print("Ola" + nome + " bem vindo")
+# print("Ola {}, você tem {} anos de idade.".format(nome,idade))
+# print(f"Ola {nome}, \nvocê tem {idade} anos de idade.")
+
+num1 = int(input("Digite um numero "))
+num2 = int(input("Digite outro numero "))
+
+# print(f"{int(num1)+int(num2)}")
+print(num1)
